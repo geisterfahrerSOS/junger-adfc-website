3243de130c52430fd03929d8178f9c5167daf61e
Marco Deuritz <marco.deuritz@gmail.com>
2026-03-11 17:27:18 +0100

    adde insta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="p-4 flex flex-col items-center gap-4">
     <div class="">
         <h1 class="font-extrabold">Hallo und Herzlich Willkommen beim Jungen ADFC Rhein-Neckar</h1>
+        <h2>Unser Instagram Kanal: <a href="https://www.instagram.com/junger_adfc_rhein_neckar/"></a></h2>
     </div>
 </div>
