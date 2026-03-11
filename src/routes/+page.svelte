9c9668c0916bcc508149d1db276b03191d906377
Marco Deuritz <marco.deuritz@gmail.com>
2026-03-11 16:29:19 +0100

    small changes to heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Hallo und Herzlich Willkommen beim Jungen ADFC Rhein-Neckar</h1>
+<div class="p-4 flex flex-col items-center gap-4">
+    <div class="">
+        <h1 class="font-extrabold">Hallo und Herzlich Willkommen beim Jungen ADFC Rhein-Neckar</h1>
+    </div>
+</div>
